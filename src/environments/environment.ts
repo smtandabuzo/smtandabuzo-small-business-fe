35e517749928e5581a6d6041fd358a8385526c37
sazi <sazi.mtandabuzo@ayo4u.com>
2025-10-01 15:47:42 +0200

    set host for alb

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: '/api' // Base API URL for development
+  apiUrl: 'https://production-alb-1780857463.eu-north-1.elb.amazonaws.com/api' // Using production API for now
 };
