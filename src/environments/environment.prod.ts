2ef0de2947396ae436433619c7c0af00ae3f22b6
sazi <sazi.mtandabuzo@ayo4u.com>
2025-10-01 16:16:53 +0200

    Use non ssl config as a temp fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 // Production environment - uses the actual API endpoint directly
 export const environment = {
   production: true,
-  apiUrl: 'https://production-alb-1780857463.eu-north-1.elb.amazonaws.com/api',
+  apiUrl: 'http://production-alb-1780857463.eu-north-1.elb.amazonaws.com/api',
   useProxy: false // No proxy in production
 };
